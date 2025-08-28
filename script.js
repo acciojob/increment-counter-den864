f6c6a198ec90fc07acc8c750e1ed781192a9da69
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-08-28 06:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const n=document.getElementById("counter");
+const btn=document.getElementById("incrementBtn");
+
+btn.addEventListener("click",()=>{
+  n.textContent=Number(n.textContent)+1;
+});
